5837afee7f6e8ed9c83a6a66aec59a23f7a6211d
Joe Ball <joe.ball@bglgroup.co.uk>
2019-10-21 14:06:57 +0100

    2 tests created and passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,13 @@
 require "oystercard"
 
 describe Oystercard do
+  let(:oystercard) { Oystercard.new }
+
+  it "creates an isntance of oystercard" do
+    expect(oystercard).to be_instance_of(Oystercard)
+  end
+
+  it "has a deafult balance of 0" do
+    expect(oystercard.balance).to eq(0)
+  end
 end
