1077685f643d59ec0a3ae74aa8155da6e863e321
Joe Ball <joe.ball@bglgroup.co.uk>
2019-10-22 14:46:19 +0100

    added station history tests and code

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :location_history
   MAXIMUM_BALANCE = 90.00
   MINIMUM_FUNDS = 1.00
+  MINIMUM_CHARGE = 1.00
 
   def initialize
     @balance = 0
     @active_journey = false
+    @location_history = []
   end
 
   def top_up(amount)
@@ -13,21 +15,27 @@ class Oystercard
     @balance += amount
   end
 
+  def in_journey?
+    @active_journey
+  end
+
+  def touch_in(station)
+    @location_history << station
+    @active_journey = true
+  end
+
+  def touch_out(amount = MINIMUM_CHARGE, station)
+    fail "Insufficient funds for journey" if @balance < MINIMUM_FUNDS
+    deduct(amount)
+    @active_journey = false
+    @location_history << station
+  end
+
+ private
+
   def deduct(amount)
     fail "Insufficient funds in balance" if @balance - amount < 0
     @balance -= amount
   end
 
-  def in_journey?
-    @active_journey
-  end
-
-  def touch_in
-    fail "Insufficient funds for journey" if @balance < MINIMUM_FUNDS
-    @active_journey = true
-  end
-
-  def touch_out
-    @active_journey = false
-  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,12 +2,13 @@ require "oystercard"
 
 describe Oystercard do
   let(:oystercard) { Oystercard.new }
+  let(:station){ double :station }
 
-  it "creates an isntance of oystercard" do
+  it "creates an instance of oystercard" do
     expect(oystercard).to be_instance_of(Oystercard)
   end
 
-  it "has a deafult balance of 0" do
+  it "has a default balance of 0" do
     expect(oystercard.balance).to eq(0)
   end
 
@@ -20,36 +21,45 @@ describe Oystercard do
     expect{oystercard.top_up(100.00)}.to raise_error "Top up maximum limit is £90.00"
   end
 
-  it "can deduct money when a ticket is bought" do
-    oystercard.top_up(10.00)
-    oystercard.deduct(3.40)
-    expect(oystercard.balance).to eq(6.60)
-  end
-
   it "can tell if you are in a journey or not" do
     expect(oystercard.in_journey?).to eq(true).or eq(false)
   end
 
   it "can track when you touch in and go on a journey" do
     oystercard.top_up(10.00)
-    oystercard.touch_in
+    oystercard.touch_in(station)
     expect(oystercard.in_journey?).to be true
   end
 
   it "can track when you touch out and are no longer on a journey" do
     oystercard.top_up(10.00)
-    oystercard.touch_out
+    oystercard.touch_out(station)
     expect(oystercard.in_journey?).to be false
   end
 
   it "throws an error if the card doesn't have enough funds for a journey" do
-    expect{oystercard.touch_in}.to raise_error("Insufficient funds for journey")
+    expect{oystercard.touch_out(station)}.to raise_error("Insufficient funds for journey")
   end
 
-  it "deducts funds when user touches in" do
+  it "deducts funds when user touches out" do
     oystercard.top_up(10.00)
-    oystercard.touch_in
-    expect(oystercard.balance).to eq(9.00)
+    expect{oystercard.touch_out(station)}.to change{oystercard.balance}.by(-Oystercard::MINIMUM_CHARGE)
   end
 
+  it "starts with an empty journey history" do
+    expect(oystercard.location_history).to be_empty
+  end
+
+  it "takes a parameter of start location when touching in" do
+    oystercard.top_up(10.00)
+    oystercard.touch_in(station)
+    expect(oystercard.location_history).to include(station)
+  end
+
+  it "takes a station as an argument and adds it to the location history" do
+    oystercard.top_up(10.00)
+    oystercard.touch_out(1.00, station)
+    expect(oystercard.location_history).to include(station)
+  end
+  
 end
